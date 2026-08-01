0980ffac81dc27f81e746153a4936bddbd6399aa
divergentolivia-del <divergentolivia@gmail.com>
2026-08-01 14:34:28 +0800

    Delete data.js

--- data.js
+++
@@ -1,9 +0,0 @@
-(function () {
-  const templateData = window.OLIVIA_TEMPLATE_DATA || { modules: [], statuses: [], audiences: [], colors: {} };
-  const userData = window.OLIVIA_USER_DATA || { itemsByModule: {} };
-
-  window.WORKBENCH_DATA = {
-    ...templateData,
-    userItemsByModule: userData.itemsByModule || {}
-  };
-})();
